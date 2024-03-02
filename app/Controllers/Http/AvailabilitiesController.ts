e3258b161938fa38fadeb668cf59d0c9240c6e29
Stefanos Michalas <82867098+StefMTP@users.noreply.github.com>
2024-03-02 21:17:26 +0200

    update availabilities

--- app/Controllers/Http/AvailabilitiesController.ts
+++ app/Controllers/Http/AvailabilitiesController.ts
@@ -1,10 +1,10 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import AvailabilityService from "App/Services/AvailabilityService";
+import AvailabilitiesService from "App/Services/AvailabilitiesService";
 
 export default class AvailabilitiesController {
   public async index({ response }: HttpContextContract) {
-    const availabilityService = new AvailabilityService();
-    const availabilities = await availabilityService.getAllAvailabilities();
+    const availabilitiesService = new AvailabilitiesService();
+    const availabilities = await availabilitiesService.getAllAvailabilities();
     return response.status(200).json({ availabilities });
   }
 }
